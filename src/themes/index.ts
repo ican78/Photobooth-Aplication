d5b03ff18ddd054b2499b7ac9d403cd921eca27b
ican78 <ikhsann78@gmail.com>
2026-07-15 17:43:33 +0700

    Delete src directory

--- src/themes/index.ts
+++
@@ -1,9 +0,0 @@
-import cyberpunk from "./cyberpunk";
-
-export const themes = {
-    cyberpunk,
-};
-
-export function getTheme(name: string) {
-    return themes[name as keyof typeof themes] ?? cyberpunk;
-}
